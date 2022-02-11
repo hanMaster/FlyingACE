7990fb208586eeb984d826d1a17a15cf21484f09
Andrey Khalepov <andrey.khalepov@latoken.com>
2022-02-12 00:41:09 +1000

    wallet connected

--- Classes/WalletButtonLayer.cpp
+++ Classes/WalletButtonLayer.cpp
@@ -1,14 +1,21 @@
 
 #include "WalletButtonLayer.h"
 #include "ui/CocosGUI.h"
+#include "wallet.h"
+#include "test.h"
 #include <iostream>
 
 USING_NS_CC;
 
 bool WalletButtonLayer::init() {
-    
-   
+    const char *seed_phrase = "pitch trust globe fish fever anchor type used aunt enemy crop spatial";
+    const char *passphrase = "localhost";
 
+    const char* signer = init_signer(seed_phrase, passphrase);
+    int balance = get_balance(signer);
+//    int balance = 5;
+
+    print_hello();
 
 	auto walletItem = MenuItemImage::create("images/back.png", "images/back.png", CC_CALLBACK_1(WalletButtonLayer::goToWelcomeCallback, this));
     walletItem->setScale(0.5);
@@ -23,7 +30,8 @@ bool WalletButtonLayer::init() {
     seedLabel->setAnchorPoint(Point(0.0f, 1.0f));
     seedLabel->setPosition(Vec2(70, 1180));
     
-    auto balanceSolLabel = Label::createWithTTF("Balance: 0", "fonts/MarkerFelt.ttf", 30);
+   
+    auto balanceSolLabel = Label::createWithTTF(std::to_string(balance), "fonts/MarkerFelt.ttf", 30);
     balanceSolLabel->enableShadow();
     balanceSolLabel->setAnchorPoint(Point(0.0f, 1.0f));
     balanceSolLabel->setPosition(Vec2(70, 980));
